461a63d70d3441c129bcce2008e3517335e23ca4
pat42410 <pat42410@hotmail.fr>
2014-09-26 16:59:50 +0200

    Design

--- controllers/depot.php
+++ controllers/depot.php
@@ -2,6 +2,8 @@
 
 class depot extends Controller {
 
+    private $depot = './depot/';
+
     public function __construct() {
         parent::__construct();
 
@@ -9,30 +11,19 @@ class depot extends Controller {
             $this->upload();
         }
         if (isset($_POST['doc'])) {
-            $this->createFolder($_POST['nameFile']);
+
+            mkdir($this->depot . $_POST['nameFile']);
         }
 
-        // Trie des fichiers par type
         $fileListe = array();
         foreach ($this->readFolder() as $file) {
-            $extension = strtolower(substr(strrchr($file, '.'), 1));
-            if (in_array($extension, array('png', 'gif', 'jpg', 'jpeg'), true)) {
-                $image = 'depot/' . $file;
-            } else {
-                $image = 'resources/icones/folder.png';
-            }
-            $fileListe[$file] = $image;
+            array_push($fileListe, $this->fileProperty($this->depot . $file));
         }
-        
+
         $variables = array(
             'fileListe' => $fileListe,
-            'path' => 'depot'
         );
         parent::callView('D&eacute;pot', $variables, array('depot.css'));
-    }
-
-    private function createFolder($name, $folder = './depot/') {
-        mkdir($folder . $name);
     }
 
     private function readFolder($folder = './depot/') {
@@ -46,34 +37,45 @@ class depot extends Controller {
         return $fileList;
     }
 
-//    private function upload() {
-//        $folder = './depot/';
-//        $sizeMax = 1000 * 1000 * 50; // En octets
-//        $extensions = array('.png', '.gif', '.jpg', '.jpeg', '.txt');
-//        $fileName = basename($_FILES['file']['name']);
-//        $fileSize = filesize($_FILES['file']['tmp_name']);
-//        $fileExtension = strrchr($_FILES['file']['name'], '.');
-//
-//        //Début des vérifications de sécurité...
-//        if (!in_array($fileExtension, $extensions)) { //Si l'extension n'est pas dans le tableau
-//            $error = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
-//        }
-//        if ($fileSize > $sizeMax) {
-//            $error = 'Le fichier est trop gros...';
-//        }
-//        if (!isset($error)) { //S'il n'y a pas d'erreur, on upload
-//            //On formate le nom du fichier ici...
-//            $fileName = strtr($fileName, 'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
-//            $fileName = preg_replace('/([^.a-z0-9]+)/i', '-', $fileName);
-//            if (move_uploaded_file($_FILES['file']['tmp_name'], $folder . $fileName)) { //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
-//                echo 'Upload effectué avec succès !';
-//            } else { //Sinon (la fonction renvoie FALSE).
-//                echo 'Echec de l\'upload !';
-//            }
-//        } else {
-//            echo $error;
-//        }
-//    }
+    private function fileProperty($name) {
+        $properties = array();
+        $stats = stat($name);
+        $properties['name'] = basename($name);
+        $properties['extension'] = strtolower(substr(strrchr($name, '.'), 1));
+        $properties['path'] = dirname($name);
+        $properties['icone'] = dirname($name) . '/' . basename($name);
+        $properties['size'] = $stats['size'];
+        return $properties;
+    }
+
+    private function upload() {
+        $folder = './depot/';
+        $sizeMax = 1000 * 1000 * 50; // En octets
+        $extensions = array('.png', '.gif', '.jpg', '.jpeg', '.txt');
+        $fileName = basename($_FILES['file']['name']);
+        $fileSize = filesize($_FILES['file']['tmp_name']);
+        $fileExtension = strrchr($_FILES['file']['name'], '.');
+
+        //Début des vérifications de sécurité...
+        if (!in_array($fileExtension, $extensions)) { //Si l'extension n'est pas dans le tableau
+            $error = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+        }
+        if ($fileSize > $sizeMax) {
+            $error = 'Le fichier est trop gros...';
+        }
+        if (!isset($error)) { //S'il n'y a pas d'erreur, on upload
+            //On formate le nom du fichier ici...
+            $fileName = strtr($fileName, 'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+            $fileName = preg_replace('/([^.a-z0-9]+)/i', '-', $fileName);
+            if (move_uploaded_file($_FILES['file']['tmp_name'], $folder . $fileName)) { //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+                echo 'Upload effectué avec succès !';
+            } else { //Sinon (la fonction renvoie FALSE).
+                echo 'Echec de l\'upload !';
+            }
+        } else {
+            echo $error;
+        }
+    }
 
     private function donwload($file) {
         $file = './depot/' . $file;
--- views/depot.php
+++ views/depot.php
@@ -1,7 +1,7 @@
 <body>
     <?php include_once './views/header.php'; ?>
 
-<!--    <form method="post" action="<?= $host ?>/depot.html" enctype="multipart/form-data">
+    <form method="post" action="<?= $host ?>/depot.html" enctype="multipart/form-data">
         Fichier : <input type="file" name="file">
         <input type="submit" name="envoyer" value="Envoyer le fichier">
     </form>
@@ -9,21 +9,18 @@
         <input type="texte" name="nameFile"/>
         <input type="submit" name="doc" value="cr&eacute;er"/>
     </form>
-    
-    <input type="texte" id="nameFile"/>
-    <button value="+" onclick="a(document.getElementById('nameFile'));">Ok</button>-->
-    
-    <?php foreach ($fileListe as $name => $image): ?>
+
+    <div class="file">
+        <img class="fileImage" src="./resources/icones/folder.png"/>
+        <span class="fileName">Fichier test</span>
+    </div>
+    <?php foreach ($fileListe as $file): ?>
         <div class="file">
-            <img class="fileImage" src="<?= $host ?>/<?= $image ?>"/>
-            <span class="fileName"><?= $name ?></span>
+            <img class="fileImage" src="./<?= $file['fullPath'] ?>"/>
+            <span class="fileName"><?= $file['name'] ?></span>
         </div>
     <?php endforeach; ?>
 
-<!--    <script type="text/javascript">
-    a(var nameFile){
-        
-    }
-    </script>-->
+
     <?php include_once './views/footer.php'; ?>
 </body>
